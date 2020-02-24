15b68931082be8fe3339a255c5e5f623f84f853a
evaldasNe <neevaldas@gmail.com>
2020-02-24 13:34:57 +0200

    project init

---
+++ New Unity Project/Assets/Script/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float jumpForce = 5f;
+    public float distToGround;
+
+    Rigidbody m_Rigidbody;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        distToGround = GetComponent<Collider>().bounds.extents.y;
+    }
+
+    //every frame
+    void Update()
+    {
+        Jump();
+    }
+
+    //every physics step
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical) * speed * Time.fixedDeltaTime;
+        Vector3 newPosition = m_Rigidbody.position + m_Rigidbody.transform.TransformDirection(movement);
+        m_Rigidbody.MovePosition(newPosition);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            m_Rigidbody.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+
+}
---
+++ New Unity Project/Assets/Script/PlayerGunController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGunController : MonoBehaviour
+{
+    float lookSensitivity = 1.5f;
+    float smoothing = 1.5f;
+
+    GameObject player;
+    Vector2 smoothedVelocity;
+    Vector2 currentLokingPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = transform.parent.gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateGun();
+        CheckForShooting();
+    }
+
+    private void RotateGun()
+    {
+        Vector2 inputValues = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        inputValues = Vector2.Scale(inputValues, new Vector2(lookSensitivity * smoothing, lookSensitivity * smoothing));
+        smoothedVelocity.x = Mathf.Lerp(smoothedVelocity.x, inputValues.x, 1f / smoothing);
+        smoothedVelocity.y = Mathf.Lerp(smoothedVelocity.y, inputValues.y, 1f / smoothing);
+
+        currentLokingPos += smoothedVelocity;
+
+        transform.localRotation = Quaternion.AngleAxis(-currentLokingPos.y, Vector3.right);
+        player.transform.localRotation = Quaternion.AngleAxis(currentLokingPos.x, player.transform.up);
+    }
+
+    private void CheckForShooting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit whatIHit;
+            if(Physics.Raycast(transform.position, transform.forward, out whatIHit, Mathf.Infinity))
+            {
+                Debug.Log(whatIHit.collider.name);
+            }
+        }
+    }
+}
