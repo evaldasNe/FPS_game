275c79d52bd1f4dc28b49ec23d12d6cc3bc86aa1
Živilė <ricardasbivainis77@gmail.com>
2020-03-09 19:12:50 +0200

    Gynimas

---
+++ New Unity Project/Assets/Script/Ball.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        m_Rigidbody.AddForce(0, 5, 0, ForceMode.Impulse);
+    }
+}
