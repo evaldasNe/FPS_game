0758732781032f6cfff3815bc72147d84e3dfd2e
evaldasNe <neevaldas@gmail.com>
2020-04-23 17:31:25 +0300

    added doors

---
+++ New Unity Project/Assets/Script/DoorScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        OpenDoors();
+    }
+
+    private void OpenDoors()
+    {
+        var transform = GetComponent<Transform>();
+        Vector3 v = new Vector3(transform.localEulerAngles.x, 90, transform.localEulerAngles.z);
+        transform.localEulerAngles = v;
+    }
+}
