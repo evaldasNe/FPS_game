984f39e2ed38b65b8a38c62585e624f3c46f5694
Gediminas Bivainis <ricardasbivainis77@gmail.com>
2020-04-24 08:32:27 +0300

    Adding animations

---
+++ New Unity Project/Assets/Script/DoorScriptSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScriptSound : MonoBehaviour
+{
+    public AudioClip sound;
+
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = sound;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<AudioSource>().Play();
+        OpenDoors();
+    }
+
+    private void OpenDoors()
+    {
+        Animator anim = GetComponent<Animator>();
+        anim.SetTrigger("OpenClose");
+
+
+    }
+}
