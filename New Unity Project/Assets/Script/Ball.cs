82a8bcd827491315c4ece6e9456d49df1af59faf
Gediminas Bivainis <ricardasbivainis77@gmail.com>
2020-05-14 10:27:44 +0300

    Defence task

--- New Unity Project/Assets/Script/Ball.cs
+++ New Unity Project/Assets/Script/Ball.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     Rigidbody m_Rigidbody;
+    float bounce = 50;
 
     void Start()
     {
@@ -12,6 +13,7 @@ public class Ball : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        m_Rigidbody.AddForce(0, 5, 0, ForceMode.Impulse);
+        m_Rigidbody.AddForce(0, bounce, 0, ForceMode.Impulse);
+        bounce= bounce - 10;
     }
 }
