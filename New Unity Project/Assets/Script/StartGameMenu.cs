f9d49da47fb600c8234882d73f0e78643acc7fd6
Gediminas Bivainis <ricardasbivainis77@gmail.com>
2020-05-14 13:28:56 +0300

    Added menu

---
+++ New Unity Project/Assets/Script/StartGameMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameMenu : MonoBehaviour
+{
+    public void PlayTutorial()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void PlayArcade()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    
+}
