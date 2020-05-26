285989448ad5c3cd2a3e5ae9ea19b08ea89ff4b7
evaldasNe <neevaldas@gmail.com>
2020-05-26 15:32:23 +0300

    done defence task

---
+++ New Unity Project/Assets/cubeScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class cubeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float yAxis = Random.Range(0f, 1f);
+        Vector3 newPosition = new Vector3(0, yAxis, 0);
+        gameObject.transform.position += newPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
